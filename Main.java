4503100ec60bb0e2b1b6f2104d1c5ceb8e10292b
hein05 <42545176+hein05@users.noreply.github.com>
2018-11-20 20:52:07 -0500

    Clean Codes

--- Main.java
+++ Main.java
@@ -19,28 +19,22 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Main main = new Main();
-		
-		// TODO Auto-generated method stub
 		ArrayStack stack = new ArrayStack();
 		CMD readWrite = new CMD("Input.json");
-//		readWrite.readFile("Input.json");
 		int count = 0;
 		String pop;
 	    while (true) {
 	        String nextCMD = readWrite.getNextCommand();   
 	        if (stack.size() > 0) {
 	        	pop = (String) stack.pop();
-	        	System.out.println("popped String >>>>>> " + pop);
 	        	if (pop.matches(".*\\btype\\b.*")) { //Check if String has "type" key
 	        		main.findData(pop);
-//	        		System.out.println("Member COUNT >>>> " + memberList.activeMemberStorage.size());
 	        	} else {
 	        		//String is not Valid. Update on Log.txt
 	        		logMessages.add("String, < " + pop + " >is not Valid. There is no \"Type\" Key");
 	        	}
 	        }
 	        if (nextCMD != null) {
-//	        	System.out.println((count++) + " " + nextCMD);
 	        	if (stack.size() < 100) {
 	        		stack.push(nextCMD);
 	        	}
@@ -48,29 +42,18 @@ public class Main {
 	        	if (stack.size() > 0) { //Last POP
 		        	stack.pop();
 		        	pop = (String) stack.pop();
-//		        	System.out.println("pop " + pop);	
 		        }
-//	        	System.out.println("size " + stack.size());
-//	        	main.writeToLog(logFile, "_______ End of Log File_________");
 	          break;
 	        }
 	      }
-	    System.out.println("ARRAY SIZE : " + memberList.activeMemberStorage.size());
-
 	    main.writeToFile(outputFile);
 	    main.writeToLog(logFile);
-//	    Main.closeLog();
-//	    stack.printArray();
 	}
 	
-//	String test  =  "\"type\":\"ADD\",\"ID\":\"10000005\",\"firstName\":\"George\",\"lastName\":\"Smith\",\"major\":\"Chemical Engineering\"";
 	enum cType {ADD, DEL, MOD, DEFAULT};
 	enum kType {ID, FIRSTNAME, LASTNAME, MAJOR, VIP, DEFAULT};
 	
 	public void findData (String popStr) {
-//		Pattern patternValue = Pattern.compile(":(.*?),"); // Find string between : and ,
-//		Pattern patternKey = Pattern.compile("\"(.*?):"); // Find string between " and :
-//		Matcher mValue = patternValue.matcher(popStr);
 		
 		cType commandType;
 		kType keyType;
@@ -101,17 +84,13 @@ public class Main {
 					commandType = cType.DEL;
 				} else {
 					//Write to log.txt
-					System.out.println("Command Not Recognized");
 					logMessages.add("Command, <"  + matchedStr + ">not recognized for <" + popStr + ">");
 					break;
 				}
 			}
 			
 			if (commandType == cType.ADD) {
-//				System.out.println("ADD String: " + matchedStr);
-//				newMember = new Member();
 				if (indexofKV > 1 && indexofKV % 2 == 0) { //Detecting if it is key
-//					System.out.println("Matched String: " + matchedStr);
 					if (matchedStr.equals("ID")) {
 						keyType = kType.ID;
 						idExists = true;
@@ -123,24 +102,18 @@ public class Main {
 						keyType = kType.MAJOR;
 					} else if (matchedStr.equals("VIP")) {
 						keyType = kType.VIP;
-					}
-//					System.out.println("Key TYPE Exists >>>> "+ indexofKV + " " + keyType + " " + matchedStr );
-					
+					}					
 				} else { //Detecting if it is Value
-//					System.out.println("Value TYPE Exists >>>> "+ indexofKV + " " + keyType + " " + matchedStr );
 					if (keyType == kType.ID) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						try { //Check if ID is integer
 							Integer.parseInt(matchedStr);
 						} catch (NumberFormatException e) { //Catching if String is not in Number Format
 							//Write to log.txt
-							System.out.println("Caught - ID is not in Number Format");
 							logMessages.add("ID not in Number Format  ->" + "For String < " + popStr);
 							break;
 						}
 						if (hashing.containsKey(matchedStr)) { // Check if key Already Exits
 							//Write to log.txt that duplicate key. Fail to write
-							System.out.println("MemberID already exists. (Suggested Command Key \"MOD\")");
 							logMessages.add("MemberID already exists. (Suggested Command Key \\\"MOD\\\")\"  ->" + "For String < " + popStr);
 							break;
 						} else {
@@ -157,27 +130,21 @@ public class Main {
 
 						}
 					} else if (keyType == kType.FIRSTNAME) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 							newMember.setfName(matchedStr);
 
 					} else if (keyType == kType.LASTNAME) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 							newMember.setlName(matchedStr);
 
 					} else if (keyType == kType.MAJOR) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 							newMember.setMajor(matchedStr);
 
 					} else if (keyType == kType.VIP) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 							newMember.setVIP(matchedStr);
 					}
 				}
 				
 			} else if (commandType == cType.MOD) {
-//				System.out.println("MOD String: " + matchedStr);
 				if (indexofKV > 1 && indexofKV % 2 == 0) { //Detecting if it is key
-//					System.out.println("Matched String: " + matchedStr);
 					if (matchedStr.equals("ID")) {
 						keyType = kType.ID;
 						idExists = true;
@@ -192,14 +159,11 @@ public class Main {
 					}
 					
 				} else { //Detecting if it is Value
-//					System.out.println("Value TYPE Exists >>>> "+ indexofKV + " " + keyType + " " + matchedStr );
 					if (keyType == kType.ID) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						try { //Check if ID is integer
 							Integer.parseInt(matchedStr);
 						} catch (NumberFormatException e) { //Catching if String is not in Number Format
 							//Write to log.txt
-							System.out.println("Caught - String is not in Number Format");
 							logMessages.add("ID not in Number Format  ->" + "For String < " + popStr);
 							break;
 						}
@@ -208,19 +172,16 @@ public class Main {
 							System.out.println(" >>>>>> " + modifiedIndex);
 						} else {
 							//Write to log New KV pairs. Wrong Command
-							System.out.println("MemberID Not exists yet. (Suggested Command Key \"ADD\")");
 							logMessages.add("MemberID already exists. (Suggested Command Key \\\"ADD\\\")\"  ->" + "For String < " + popStr);
 							break;
 						}
 					} else if (keyType == kType.FIRSTNAME) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						if (modifiedIndex != null) {
 							String fName = memberList.activeMemberStorage.get(modifiedIndex).getFirstName();
 							memberList.activeMemberStorage.get(modifiedIndex).setfName(matchedStr);
 							logMessages.add("Change Member First Name From:  ->" + fName + " To " + matchedStr);
 						}
 					} else if (keyType == kType.LASTNAME) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						if (modifiedIndex != null) {
 							String lName = memberList.activeMemberStorage.get(modifiedIndex).getLastName();
 							memberList.activeMemberStorage.get(modifiedIndex).setlName(matchedStr);
@@ -228,7 +189,6 @@ public class Main {
 						}
 
 					} else if (keyType == kType.MAJOR) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						if (modifiedIndex != null) {
 							String getMajor = memberList.activeMemberStorage.get(modifiedIndex).major();
 							memberList.activeMemberStorage.get(modifiedIndex).setMajor(matchedStr);
@@ -236,7 +196,6 @@ public class Main {
 						}
 
 					} else if (keyType == kType.VIP) {
-//						System.out.println("ValueType Exists >>>> " + matchedStr);
 						if (modifiedIndex != null) {
 							memberList.activeMemberStorage.get(modifiedIndex).setVIP(matchedStr);
 							logMessages.add("Change Member's VIP status");
@@ -245,7 +204,6 @@ public class Main {
 				}
 				
 			} else if (commandType == cType.DEL) {
-//				System.out.println("DEL String: " + matchedStr);
 				if (indexofKV == 2) {
 					if (matchedStr.equals("ID")) {
 						keyType = kType.ID;
@@ -257,23 +215,18 @@ public class Main {
 						Integer.parseInt(matchedStr);
 					} catch (NumberFormatException e) { //Catching if String is not in Number Format
 						//Write to log.txt
-						System.out.println("Caught - String is not in Number Format");
 						logMessages.add("ID not in Number Format  ->" + "For String < " + popStr);
-
 						break;
 					}
 					if (hashing.containsKey(matchedStr)) { // Check if key Must Already Exits
 						modifiedIndex = hashing.find(matchedStr);
 						memberList.activeMemberStorage.set(modifiedIndex, null);
-//						memberList.activeMemberStorage.add(modifiedIndex, null);
 						logMessages.add("REMOVED Hashing Index  ->" + "For ID < " + matchedStr);
 						hashing.remove(matchedStr);
 						memberList.removedMemberStorage.add(modifiedIndex);
 						logMessages.add("REMOVED Member from ArrayList  ->" + "For Index < " + modifiedIndex);
-//						System.out.println(" Removed >>>>>> " + modifiedIndex + " " + memberList.activeMemberStorage.get(modifiedIndex));
 					} else {
 						//Write to log New KV pairs. Wrong Command
-//						System.out.println("MemberID Not exists yet. (Suggested Command Key \"ADD\")");
 						logMessages.add("MemberID already Not exists. (Suggested Command Key \\\"ADD\\\")\"  ->" + "For String < " + popStr);
 						break;
 					}
@@ -286,14 +239,9 @@ public class Main {
 		if (indexofKV % 2 == 0 && idExists) { // CHECKING IF STRING IS CORRUPTED BY CHECKING EVEN PAIRS
 			if (commandType == cType.ADD) {
 				if (memberList.removedMemberStorage.size() > 0) {
-					System.out.println("Member removedList" + memberList.removedMemberStorage.get(0));
 					memberList.activeMemberStorage.set(memberList.removedMemberStorage.get(0), newMember);
-//					memberList.activeMemberStorage.add(memberList.removedMemberStorage.get(0), newMember);
-//					memberList.activeMemberStorage.remove(memberList.removedMemberStorage.get(0) + 1);
 					memberList.removedMemberStorage.remove(0);
-					System.out.println("Member removedList" + memberList.activeMemberStorage.get(2));
 				} else {
-					System.out.println("Member INFO ADDED >>>> " + "Index of KV = " + indexofKV + ">>>"+ newMember.getMemberInfo() + " Hashed Value >>>> " + hashing.find(newMember.getCunyID()));
 					memberList.activeMemberStorage.add(newMember);
 				}
 				logMessages.add("ADDED new Member to ArrayList  ->" + "Index of KV = " + indexofKV + ">>>"+ newMember.getMemberInfo() + " Hashed Value >>>> " + hashing.find(newMember.getCunyID()) );
@@ -302,7 +250,6 @@ public class Main {
 			}
 		} else {
 			if (!idExists) {
-				System.out.println("NO MEMBER-ID found for String" + popStr);
 				//Write to Log.txt
 				logMessages.add("NO MEMBER-ID found for String" + popStr);
 			} else {
@@ -310,23 +257,7 @@ public class Main {
 				logMessages.add("KV PAIRS STRING CORRUPTED.");
 			}
 		}
-				
-//		if (mValue.find()) {
-//			
-//		    System.out.println("Finding value >>>> " + mValue.group(1));
-//		    
-////		    System.out.println("your \"string\" here".split("\"")[1]);
-//		    String str = mValue.group(1);
-//		    Pattern p = Pattern.compile("\"([^\"]*)\""); //Finding string between quotes
-//		    Matcher match = p.matcher(str);
-//		    while (match.find()) {
-//		      System.out.println("FOUND INSIDE QUOTE >>>>" + match.group(1));
-//		    }
-//		    System.out.println();
-//		}
-//		if (mValue.find()) {
-//			System.out.println("Finding Next Value >>>> " + mValue.group(1));
-//		}	
+					
 	}
 	
 	
